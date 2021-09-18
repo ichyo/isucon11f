e6382b88ebb474d2799bd3dd55dd7513df8dbb16
ichyo <ichyo00@gmail.com>
2021-09-18 16:27:40 +0900

    try

--- go/main.go
+++ go/main.go
@@ -1151,13 +1151,13 @@ func (h *handlers) SubmitAssignment(c echo.Context) error {
 		return c.NoContent(http.StatusInternalServerError)
 	}
 
-	dst := AssignmentsDirectory + classID + "-" + userID + ".pdf"
-	if err := os.WriteFile(dst, data, 0666); err != nil {
+	if err := tx.Commit(); err != nil {
 		c.Logger().Error(err)
 		return c.NoContent(http.StatusInternalServerError)
 	}
 
-	if err := tx.Commit(); err != nil {
+	dst := AssignmentsDirectory + classID + "-" + userID + ".pdf"
+	if err := os.WriteFile(dst, data, 0666); err != nil {
 		c.Logger().Error(err)
 		return c.NoContent(http.StatusInternalServerError)
 	}
